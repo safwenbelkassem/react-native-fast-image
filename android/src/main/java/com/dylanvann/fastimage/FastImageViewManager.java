171119681f22aaa5d20533c76b97d966af44d119
safwenbelkassem <45638673+safwenbelkassem@users.noreply.github.com>
2021-06-04 15:36:46 +0100

    fix Image disappears when using react-native-screens

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
@@ -20,7 +20,7 @@ import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.List; 
 import java.util.Map;
 import java.util.WeakHashMap;
 
